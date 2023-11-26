cb1caaa091063209062ba6ff879c357c9b054ddf
Mihai-Maxim <135857206+mihaimaximthinslices@users.noreply.github.com>
2023-11-26 14:20:13 +0200

    Clean up stack

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,8 +1,3 @@
-import "@testing-library/cypress/add-commands";
-import { registerCommands } from "./commands";
-
-registerCommands();
-
 Cypress.on("uncaught:exception", (err) => {
   // Cypress and React Hydrating the document don't get along
   // for some unknown reason. Hopefully we figure out why eventually
